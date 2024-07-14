ee91669fb12dc65c02800ffdec9df83b4eca63bd
augustinekudiyirippil <augustinekudiyirippil@gmail.com>
2024-07-14 12:38:37 +0100

    Updated SQL tables

--- SQL_FacilityManagementSystem.sql
+++ SQL_FacilityManagementSystem.sql
@@ -1,99 +1,129 @@
- 
 
-create table tblRoles
-(
-roleID UUID primary key,
-roleNumber int,
-roleName varchar(50),
-roleIsDeleted int default 0
-);
-
-
-create table tblFacilityTypes
-(
-fltyTypeID UUID primary key,
-fltyTypeName varchar(50),
-fltyTypeDescription varchar(500),
-fltyIsDeleted int default 0
-);
-
-create table tblUsers
-(
-usrID uuid primary key,
-usrNumber int,
-usrFirstName varchar(50),
-usrLastname varchar(50),
-usrKnowenAs varchar(50),
-usrAddress varchar(250),
-usrPrimaryEmail varchar(50),
-usrSecondaryEmail varchar(50),
-usrMobile varchar(50),
-usrWhatsApp varchar(50),
-usrLandphone varchar(50),
-usrMFAActivated int default 0,
-usrSSOActivated int default 0,
-usrNotes varchar(250),
-usrIsDeleted int,
-);
 
  
 
-create table tblFacilities
-(
-fltyID uuid primary key,
-fltyNumber int,
-fltyType UUID REFERENCES tblFacilityTypes (fltyTypeID),
-fltyOwner UUID REFERENCES tblUsers  (usrID),
-fltyName varchar(50),
-fltyDescription varchar(500),
-fltyAddress varchar(250),
-fltyTelephone varchar(50),
-fltyEmail varchar(50),
-fltyWebsite varchar(50),
-fltyBankName varchar(100),
-fltyBankAccountName varchar(50),
-fltyBankAccountNumber varchar(40),
-fltyBankSortCode varchar(10),
-fltyBankIFSCCode varchar(25),
-fltyBankIBAN varchar(50),
-fltyIsDeleted int default 0,
-fltyActive int default 0
+CREATE TABLE public.tblroles (
+	roleid uuid NOT NULL,
+	rolenumber int4 DEFAULT nextval('rolenumberseq'::regclass) NULL,
+	rolename varchar(50) NULL,
+	roleisdeleted int4 DEFAULT 0 NULL,
+	CONSTRAINT tblroles_pkey PRIMARY KEY (roleid)
 );
 
 
  
 
-create table tblFacilityAttachments
-(
-fltyAttachmentID uuid primary key,
-fltyID  UUID REFERENCES tblFacilities (fltyID),
-fltyAttachmentName varchar(250),
-fltyAttachmentPath varchar(250),
-fltyAttachmentIsDeleted int default 0,
-fltyAttachmentApproved int default 0
+
+
+CREATE TABLE public.tblusers (
+	usrid uuid NOT NULL,
+	usrnumber int4 DEFAULT nextval('usrnumberseq'::regclass) NULL,
+	usrfirstname varchar(50) NULL,
+	usrlastname varchar(50) NULL,
+	usrknownas varchar(50) NULL,
+	usraddress varchar(250) NULL,
+	usrprimaryemail varchar(50) NULL,
+	usrsecondaryemail varchar(50) NULL,
+	usrmobile varchar(50) NULL,
+	usrwhatsapp varchar(50) NULL,
+	usrlandphone varchar(50) NULL,
+	usrmfaactivated int4 DEFAULT 0 NULL,
+	usrssoactivated int4 DEFAULT 0 NULL,
+	usrnotes varchar(250) NULL,
+	usrisdeleted int4 NULL,
+	usrroledid uuid NULL,
+	CONSTRAINT tblusers_pkey PRIMARY KEY (usrid)
 );
-
-
-create table tblFacilityBooking
-(
-fltyBookingID uuid primary key,
-fltyBookingNumber int ,
-fltyBookindOn	 timestamp,
-fltyBookedBy UUID REFERENCES tblUsers  (usrID),
-fltyBookingDate timestamp,
-fltyBookingHoursFrom int,
-fltyBookingHoursTo int,
-fltyBookingApproved int default 0 ,
-fltyBookingApprovedOn timestamp,
-fltyBookingIsDeleted int default 0 ,
-fltyBookingNotes varchar(250),
-fltyBookingPaid int default 0 
-);
-
-
-
-
-
 
 
  
+
+ALTER TABLE public.tblusers ADD CONSTRAINT tblusers_usrroledid_fkey FOREIGN KEY (usrroledid) REFERENCES public.tblroles(roleid);
+
+
+ 
+
+CREATE TABLE public.tblfacilitytypes (
+	fltytypeid uuid NOT NULL,
+	fltytypename varchar(50) NULL,
+	fltytypedescription varchar(500) NULL,
+	fltyisdeleted int4 DEFAULT 0 NULL,
+	CONSTRAINT tblfacilitytypes_pkey PRIMARY KEY (fltytypeid)
+);
+
+
+ 
+
+CREATE TABLE public.tblfacilities (
+	fltyid uuid NOT NULL,
+	fltynumber int4 DEFAULT nextval('fltynumberseq'::regclass) NULL,
+	fltytype uuid NULL,
+	fltyowner uuid NULL,
+	fltyname varchar(50) NULL,
+	fltydescription varchar(500) NULL,
+	fltyaddress varchar(250) NULL,
+	fltytelephone varchar(50) NULL,
+	fltyemail varchar(50) NULL,
+	fltywebsite varchar(50) NULL,
+	fltybankname varchar(100) NULL,
+	fltybankaccountname varchar(50) NULL,
+	fltybankaccountnumber varchar(40) NULL,
+	fltybanksortcode varchar(10) NULL,
+	fltybankifsccode varchar(25) NULL,
+	fltyisdeleted int4 DEFAULT 0 NULL,
+	fltyactive int4 DEFAULT 0 NULL,
+	fltybankiban varchar(50) NULL,
+	fltyrate float8 NULL,
+	fltycurrency varchar(10) NULL,
+	CONSTRAINT tblfacilities_pkey PRIMARY KEY (fltyid)
+);
+
+
+
+ALTER TABLE public.tblfacilities ADD CONSTRAINT tblfacilities_fltyowner_fkey FOREIGN KEY (fltyowner) REFERENCES public.tblusers(usrid);
+ALTER TABLE public.tblfacilities ADD CONSTRAINT tblfacilities_fltytype_fkey FOREIGN KEY (fltytype) REFERENCES public.tblfacilitytypes(fltytypeid);
+
+
+ 
+
+CREATE TABLE public.tblfacilityattachments (
+	fltyattachmentid uuid NOT NULL,
+	fltyid uuid NULL,
+	fltyattachmentname varchar(250) NULL,
+	fltyattachmentpath varchar(250) NULL,
+	fltyattachmentapproved int4 DEFAULT 0 NULL,
+	fltyattachmentisdeleted int4 DEFAULT 0 NULL,
+	CONSTRAINT tblfacilityattachments_pkey PRIMARY KEY (fltyattachmentid)
+);
+
+
+ 
+
+ALTER TABLE public.tblfacilityattachments ADD CONSTRAINT tblfacilityattachments_fltyid_fkey FOREIGN KEY (fltyid) REFERENCES public.tblfacilities(fltyid);
+
+
+ 
+CREATE TABLE public.tblfacilitybooking (
+	fltybookingid uuid NOT NULL,
+	fltybookingnumber int4 DEFAULT nextval('fltybookingnumberseq'::regclass) NULL,
+	fltybookedon timestamp NULL,
+	fltybookedby uuid NULL,
+	fltybookingdate timestamp NULL,
+	fltybookinghoursfrom int4 NULL,
+	fltybookinghoursto int4 NULL,
+	fltybookingapproved int4 DEFAULT 0 NULL,
+	fltybookingapprovedon timestamp NULL,
+	fltybookingisdeleted int4 DEFAULT 0 NULL,
+	fltybookingnotes varchar(250) NULL,
+	fltybookingpaid int4 DEFAULT 0 NULL,
+	fltyid uuid NULL,
+	fltybookingrate float8 NULL,
+	fltybookingamountpaid float8 NULL,
+	CONSTRAINT tblfacilitybooking_pkey PRIMARY KEY (fltybookingid)
+);
+
+
+ 
+ALTER TABLE public.tblfacilitybooking ADD CONSTRAINT tblfacilitybooking_fltybookedby_fkey FOREIGN KEY (fltybookedby) REFERENCES public.tblusers(usrid);
+ALTER TABLE public.tblfacilitybooking ADD CONSTRAINT tblfacilitybooking_fltyid_fkey FOREIGN KEY (fltyid) REFERENCES public.tblfacilities(fltyid);
+
